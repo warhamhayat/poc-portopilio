71061fcb58ddaa54812e3262d6b696033bcf091a
warhamhayat <warhamhayat@gmail.com>
2025-05-05 13:39:47 +0700

    repo after build

--- src/component/hero.jsx
+++ src/component/hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className='bg-emerald-100 py-32' id='home'>
         <div className='align-element grid md:grid-cols-2 items-center gap-8'>
             <article>
-                <h1 className='text-7xl font-bold tracking-wider'>I'm <span className='text-yellow-500'>Toys</span></h1>
+                <h1 className='text-7xl font-bold tracking-wider'>I'm <span className='text-yellow-500'>Farhanaaa</span></h1>
                 <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide'>Infra Cloud Engineer</p>
                 <p className='mt-2 text-lg text-slate-700 capitalize tracking-wide'>build your app scalably, securely and reliably</p>
                 <div className='flex gap-x-4 mt-4'>
